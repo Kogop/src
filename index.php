d5767898dd0dc40d5720bfdbbaf0aa0b574a5439
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-11-13 22:15:47 +0300

    addlittle more understanding of what Im tryingtodo

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-
-   <?php echo "Hello world"; ?>
-
+   <?php  
+    include_once('htmlpart.php');
+   ?>
+    
    
-  </body>
-</html>
+ 
