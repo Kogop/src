22b0671690ea07c1c14a58b858e084d90d6d18ba
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-11-16 17:28:31 +0300

    popka

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-
    <?php  
-    require('htmlpart/htmlpart.php');
-
-
-
+   
+    require_once('./../htmlpart/htmlpart.php');
+      $title = "hello, world";
+      $connect = mysqli_connect("localhost", "root", "Idropmydick23", "exams");
+      if (!$connect) {
+         echo "ERROR: Can't connect to database";
+      }
 
 
 
