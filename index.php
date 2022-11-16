54e287cad42b3e9e1846b9b42b9294fbb3046db7
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-11-17 00:20:41 +0300

    works, just need to do actual job

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
    
     require_once('./htmlpart/htmlpart.php');
       $title = "hello, world";
+      #print_r(get_loaded_extensions());
+      #phpinfo(1);
       $connect = mysqli_connect("localhost", "root", "Idropmydick23", "exams");
       if (!$connect) {
          echo "ERROR: Can't connect to database";
