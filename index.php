d127aad36857a9c483037dc8e3e6bfda42792289
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-11-17 23:39:41 +0300

    front is not for me

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
-   <?php  
-   
-    require_once('./htmlpart/htmlpart.php');
-      $title = "hello, world";
-      #print_r(get_loaded_extensions());
-      #phpinfo(1);
-      $connect = mysqli_connect("localhost", "root", "Idropmydick23", "exams");
-      if (!$connect) {
-         echo "ERROR: Can't connect to database";
-      }
+   <?php
+
+   require_once('./htmlpart/htmlpart.php');
+   $title = "hello, world";
+   #print_r(get_loaded_extensions());
+   #phpinfo(1);
+   $connect = mysqli_connect("localhost", "root", "Idropmydick23", "exams");
+   if (!$connect) {
+      die("Connection failed: " . mysqli_connect_error());
+   }
 
 
-
-
-
-
-
-
-
-
-    
+   mysqli_close($connect);
    ?>
     
    
