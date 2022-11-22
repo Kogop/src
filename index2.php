af76eac7dc3d5935497d24134c416d10439b04ab
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-11-22 20:33:46 +0300

    make normal out, now only add func is needed

--- index2.php
+++ index2.php
@@ -30,6 +30,19 @@
    
       if ($result->num_rows > 0) {
      // output data of each row
+
+     $out = "<div class="."calendar-item>"."   
+     <div class="."calendar-head".">Exams</div>
+     <table>
+     <tr>
+       <th>Nazv predmeta</th>
+       <th>DATA AND VREMYA</th>
+       <th>KODE ITEM</th>
+       <th>NAZV SPEC</th>
+     </tr>
+     <table>
+        <tr>
+        ";
          while($row = $result->fetch_assoc()) {     // TODO: когда две и > cпециальностей на одну дату, то заново создается шапка и название таблицы, переделать
             #$number = $row["id_subject"];
            # $sql_1 = "SELECT `title` FROM `subjects` WHERE id = ". $number. ";";
@@ -41,28 +54,19 @@
             #$sql_3 = "SELECT * FROM `schedule` WHERE DAYOFMONTH(`date`) = ".$_GET['day'].";";
             #$result_3 = $conn->query($sql_3);
             # <td class="."calendar-day ".">" . $row["id"]. "</td>
+               // переделать этот эко, таким образом, что этот текст записывается в переменную один раз, а потом только лишь добавляется новое, видимо надо каунтер делать.
 
-       echo "<div class="."calendar-item>"."   
-         <div class="."calendar-head".">Exams</div>
-         <table>
-         <tr>
-           <th>Nazv predmeta</th>
-           <th>DATA AND VREMYA</th>
-           <th>KODE ITEM</th>
-           <th>NAZV SPEC</th>
-         </tr>
-         <table>
-            <tr>
-            
+            $out .= "
             <td class="."calendar-day "."> " . $row["title"]. "</td>
             <td class="."calendar-day "."> " . $row["date"]. "</td>
             <td class="."calendar-day "."> " . $row["id_speciality"]. "</td>
             <td class="."calendar-day "."> " . $row["nazv spec"]. "</td>
-            <br>
-            </table>
-            </div>
-            </div>";
+            </tr>";
          }
+         $out .= "</table>
+         </div>
+         </div>";
+         echo $out;
       } else {
       echo "0 results";
       }
